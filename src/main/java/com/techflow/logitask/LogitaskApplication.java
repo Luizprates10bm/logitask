1c670ad093cc54ab8ad378fbc82ae0e850171633
Luizprates10bm <luizgustavoaugustoprates@gmail.com>
2026-07-07 15:59:21 -0300

    Criando a classe principal LogitaskApplication para inicializacao do Spring Boot

---
+++ src/main/java/com/techflow/logitask/LogitaskApplication.java
@@ -0,0 +1,12 @@
+package com.techflow.logitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogitaskApplication {
+    // Método principal que serve como ponto de entrada para rodar o sistema
+    public static void main(String[] args) {
+        SpringApplication.run(LogitaskApplication.class, args);
+    }
+}
